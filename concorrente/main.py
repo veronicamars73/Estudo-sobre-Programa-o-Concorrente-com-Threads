8a81382453f4f9bed56728c36dc6b8d5d88ef6c9
Lisandra Melo <mendie73@gmail.com>
2022-10-05 18:08:41 -0300

    Atualização para evitar erros

--- concorrente/main.py
+++ concorrente/main.py
@@ -1,16 +1,27 @@
 
 import json
+from logging import exception
 from threading import Thread
 import requests
 import time
 import pandas as pd
-
+import contextlib
+import io
 
 def run_thread(url,headers,querystring):
     inicial = time.time()
     test = True
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    time.sleep(.07)
+    try:
+        with contextlib.redirect_stderr():
+            response = requests.request("GET", url, headers=headers, params=querystring)
+    except Exception as ex:
+        try:
+            with contextlib.redirect_stderr():
+                response = requests.request("GET", url, headers=headers, params=querystring)
+        except Exception as e:
+            time.sleep(.5)
+            with contextlib.redirect_stderr(io.StringIO()):
+                response = requests.request("GET", url, headers=headers, params=querystring)
     json_obj = json.loads(response.text)
     print("Temperatura atual é de {}ºC e a sensação térmica é de {}ºC.".format(json_obj['current']['temp_c'],json_obj['current']['feelslike_c']))
     final = time.time()
@@ -21,13 +32,12 @@ url = "https://weatherapi-com.p.rapidapi.com/current.json"
 querystring = {"q":"-5.79448, -35.211"}
 
 headers = {
-        "X-RapidAPI-Key": "e8a6292771msh67289abb8e0572fp1e1f25jsn551312caa50c",
+        "X-RapidAPI-Key": "e17e1e7f59mshca729c8a42e65c6p16a703jsn7d1d2939ef7c",
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
         'User-agent': 'Mozilla/5.0'
     }
 
-cenarios = [10, 50, 100, 200, 500, 1000, 5000]
-
+cenarios = [10, 50, 100, 200, 500, 1000]
 dados = {'num_execucao':[], 'n':[], 'tempo':[]}
 
 for c in cenarios:
